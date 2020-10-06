2c74976f562edfd0a1f88e457d4e1c65931cfdad
ShoaibSikander <shoaib.pu@gmail.com>
2020-10-06 16:17:58 +0200

    Comments Inserted

--- CPP_Calculator_Application/calculator.cpp
+++ CPP_Calculator_Application/calculator.cpp
@@ -4,86 +4,86 @@
 
 using namespace std;
 
-Calc::Calc(int value)
+Calc::Calc(int value)															// Constructor
 {
-	storedExpressions = new string[value];
-	index = 0;
+	storedExpressions = new string[value];										// Make an array of user defined size and assign to pointer
+	index = 0;																	// Initialize the array index number with a zero value
 }
 
-Calc::~Calc()
+Calc::~Calc()																	// Destructor
 {
-	delete[] storedExpressions;
-	storedExpressions = nullptr;
-	index = 0;
+	delete[] storedExpressions;													// Delete the array which is made in the Constructor
+	storedExpressions = nullptr;												// Set it equal to the Null Pointer
+	index = 0;																	// Make the array index number equal to Zero
 }
 
-double Calc::process(string userInput, double prevValue)
+double Calc::process(string userInput, double prevValue)						// Function for mathematical calculation
 {
-	string cleansedString = "";
-	for (size_t i = 0; i < userInput.size(); ++i)
+	string cleansedString = "";													// Define and initialize an empty string
+	for (size_t i = 0; i < userInput.size(); ++i)								// Run the for-loop equal to string's size
 	{
-		if (userInput[i] == '0' || userInput[i] == '1' || userInput[i] == '2' || userInput[i] == '3' || userInput[i] == '4' || userInput[i] == '5' || userInput[i] == '6' || userInput[i] == '7' || userInput[i] == '8' || userInput[i] == '9' || userInput[i] == '+' || userInput[i] == '-' || userInput[i] == '*' || userInput[i] == '/' || userInput[i] == '^' || userInput[i] == '.')
+		if (userInput[i] == '0' || userInput[i] == '1' || userInput[i] == '2' || userInput[i] == '3' || userInput[i] == '4' || userInput[i] == '5' || userInput[i] == '6' || userInput[i] == '7' || userInput[i] == '8' || userInput[i] == '9' || userInput[i] == '+' || userInput[i] == '-' || userInput[i] == '*' || userInput[i] == '/' || userInput[i] == '^' || userInput[i] == '.')  // if its a number or correct operation sign 
 		{
-			cleansedString += userInput[i];
+			cleansedString += userInput[i];										// Connect the correct values in a string
 		}
-		else
-		{
-		}
-	}
-	int stringLength = cleansedString.length() - 1;
-	string cutString = cleansedString.substr(1, stringLength);
-	double inputValue = stod(cutString);
-	if (cleansedString[0] == '^')
-	{
-		return pow(prevValue, inputValue);
-	}
-	else if (cleansedString[0] == '+')
-	{
-		return prevValue + inputValue;
-	}
-	else if (cleansedString[0] == '-')
-	{
-		return prevValue - inputValue;
-	}
-	else if (cleansedString[0] == '*')
-	{
-		return prevValue * inputValue;
-	}
-	else if (cleansedString[0] == '/')
-	{
-		if (inputValue == 0)
-		{
-			exit(0);
-		}
-		else
-		{
-			return prevValue / inputValue;
+		else																	// Otherwise if there is some incorrect value input by user
+		{																		// Ignore that value	
 		}
 	}
-	else
+	int stringLength = cleansedString.length() - 1;								// Find the length of newly constructed array and subtract 1
+	string cutString = cleansedString.substr(1, stringLength);					// Ignore the symbol at first location of array
+	double inputValue = stod(cutString);										// Convert the string array to numeric value
+	if (cleansedString[0] == '^')												// If the first value of the input string is ^
 	{
-		cout << "Error: Must start with operator" << endl;
+		return pow(prevValue, inputValue);										// result = (AccumulatedAnswer)^(InputValue)
+	}
+	else if (cleansedString[0] == '+')											// If the first value of the input string is +
+	{
+		return prevValue + inputValue;											// result = (AccumulatedAnswer)+(InputValue)
+	}
+	else if (cleansedString[0] == '-')											// If the first value of the input string is -
+	{
+		return prevValue - inputValue;											// result = (AccumulatedAnswer)-(InputValue)
+	}
+	else if (cleansedString[0] == '*')											// If the first value of the input string is *
+	{
+		return prevValue * inputValue;											// result = (AccumulatedAnswer)*(InputValue)
+	}
+	else if (cleansedString[0] == '/')											// If the first value of the input string is /
+	{
+		if (inputValue == 0)													// If the input string is zero
+		{
+			exit(0);															// Exit the code													
+		}
+		else																	// If the input string is non-zero
+		{
+			return prevValue / inputValue;										// result = (AccumulatedAnswer)/(InputValue)
+		}
+	}
+	else																		// Otherwise
+	{
+		cout << "Error: Must start with operator" << endl;						// Print on the Console
 	}
 
 }
 
-void Calc::print()
+void Calc::print()																// Print function
 {
-	cout << endl << "******************************" << endl;
-	cout << "You have " << index << " expressions stored ...!!!" << endl;
-	for (size_t i = 0; i < index; ++i)
+	cout << endl << "******************************" << endl;					// Print on the Console
+	cout << "You have " << index << " expressions stored ...!!!" << endl;		// Print on the Console
+	for (size_t i = 0; i < index; ++i)											// Start for-loop
 	{
-		cout << "Expression " << i + 1 << ": " << storedExpressions[i] << endl;
+		cout << "Expression " << i + 1 << ": " << storedExpressions[i] << endl;	// Print on the Console
 	}
-	cout << endl << "******************************" << endl;
+	cout << endl << "******************************" << endl;					// Print on the Console
 }
 
-void Calc::incrementArrayIndex(size_t currentIndex)
+void Calc::incrementArrayIndex(size_t currentIndex)									// Function to move the index to next position in array
 {
-	index = currentIndex +1;
+	index = currentIndex +1;													// New_Index = Old_Index + 1
 }
 
-size_t Calc::getArrayIndex()
+size_t Calc::getArrayIndex()													// Function to return the current index value of array
 {
-	return index;
+	return index;																// Return the current value of array index
 }
--- CPP_Calculator_Application/main.cpp
+++ CPP_Calculator_Application/main.cpp
@@ -5,54 +5,54 @@ using namespace std;
 
 int main()
 {
-    int sentinal = 0;
-    do
-    {
-        int numExp;
-        cout << "Please enter the number of expressions you would like to store: ";
-        cin >> numExp;
-        if (numExp < 1)
+    int sentinal = 0;                                                           // Initialize the variable for the do-while loop
+    do                                                                          // Start the Do-While loop
+    {                                                               
+        int numExp;                                                             // Variable to store the number of expressions
+        cout << "Please enter the number of expressions you would like to store: ";            // Print on the Console
+        cin >> numExp;                                                          // Take user input for the number of expressions
+        if (numExp < 1)                                                         // If user enters a value less than 1, then its not realistic
         {
-            cerr << "Error: Number of stored expressions must be positive ...!!!" << endl;
+            cerr << "Error: Number of stored expressions must be positive ...!!!" << endl;     // Print on the Console
         }
-        else
+        else                                                                    // If user enters a value equal to 1 or greater
         {
-            Calc calculator_obj = Calc(numExp);
+            Calc calculator_obj = Calc(numExp);                                 // Initialize the Class object and call its constructor
 //            Calc abc = Calc(calculator_obj);
-            for (size_t count = 0; count < numExp; count++)
+            for (size_t count = 0; count < numExp; count++)                     // Run the loop for the user defined number of expressions
             {
-                cout << endl << "******************************" << endl;
-                cout << "Expression No. " << count+1 << " started ...!!!" << endl;
-                int currentIndex = 0;
-                double result = 0;
-                double answer = 0;
-                bool flag = true;
-                while (flag)
+                cout << endl << "******************************" << endl;       // Print on the Console
+                cout << "Expression No. " << count+1 << " started ...!!!" << endl;  // Print on the Console
+                int currentIndex = 0;                                           // Declare and initialize the variable to store the current index of array where result will be stored
+                double result = 0;                                              // Declare and initialize the variable to store the mathematical result
+                double answer = 0;                                              // Declare and initialize the variable to store the accumulated answer
+                bool flag = true;                                               // Define a boolean variable and initialize it with "true"                                
+                while (flag)                                                    // Keep running the While-Loop untill the flag is true
                 {
-                    cout << "------------------------------" << endl;
-                    string userString;
-                    cout << "Enter 'STOP' to halt the input stream" << endl;
-                    cout << "Accumulated Answer: " << answer << endl;
-                    cout << "Please enter the number: ";
-                    cin >> userString;
-                    if (userString == "STOP")
+                    cout << "------------------------------" << endl;           // Print on the Console
+                    string userString;                                          // Define string variable to get user input number for calculation
+                    cout << "Enter 'STOP' to halt the input stream" << endl;    // Print on the Console
+                    cout << "Accumulated Answer: " << answer << endl;           // Print on the Console
+                    cout << "Please enter the number: ";                        // Print on the Console
+                    cin >> userString;                                          // Get user input for calculation
+                    if (userString == "STOP")                                   // If user enters "STOP", then this if-condition will be executed
                     {
-                        currentIndex = calculator_obj.getArrayIndex();
-                        calculator_obj.storedExpressions[currentIndex] = to_string(answer);
-                        flag = false;
-                        break;
+                        currentIndex = calculator_obj.getArrayIndex();          // Get the current index number where accumulated answer will be stored
+                        calculator_obj.storedExpressions[currentIndex] = to_string(answer); // Convert the accumulated answer to string and store at the first available index of array
+                        flag = false;                                           // Set the flag to False
+                        break;                                                  // Break the loop
                     }
-                    result = calculator_obj.process(userString, answer);
-                    cout << answer << userString << " = " << result << endl;
-                    answer = result;
+                    result = calculator_obj.process(userString, answer);        // Perform mathematical calculations and get the result
+                    cout << answer << userString << " = " << result << endl;    // Display the calculated result on the Console
+                    answer = result;                                            // Overwrite the value of accumuated answer
                 }
-                calculator_obj.incrementArrayIndex(currentIndex);
+                calculator_obj.incrementArrayIndex(currentIndex);               // Increase index to store the next calculated answer 
             }
-            calculator_obj.print();
-            std::cout << "Press 1 to continue with another calculator, any other value to quit" << std::endl;
-            std::cin >> sentinal;
-            cout << "------------------------------" << endl;
+            calculator_obj.print();                                             // Call the "print" function of the Class and display accumulated answers for all expressions
+            std::cout << "Press 1 to continue with another calculator, any other value to quit" << std::endl;   // Print on the Console 
+            std::cin >> sentinal;                                               // Take the user input and check if he/she wants to continue or not
+            cout << "------------------------------" << endl;                   // Print on the Console
         }
-    } while (sentinal == 1);
-    return 0;
+    } while (sentinal == 1);                                                    // Check the condition for the While Loop
+    return 0;                                                                   // Main function finished without any error
 }
